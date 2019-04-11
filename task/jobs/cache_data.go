dd265dc7cfda536e361df28d6a9638578ba7fd76
Shonminh Yang <shonminh@gmail.com>
2019-04-12 03:13:34 +0800

    add cache data job

---
+++ task/jobs/cache_data.go
@@ -0,0 +1,21 @@
+// Copyright (c) 2019. Shonminh Yang
+// db-cacher framework source code and usage is governed by a MIT style license that can be found in the LICENSE file.
+
+package jobs
+
+func CacheData() error {
+
+	//TODO implement me
+
+	return nil
+}
+
+func ScanDB() interface{} {
+	//TODO implement me
+	return nil
+}
+
+func SyncData(data interface{}) error {
+	//TODO implement me
+	return nil
+}
